4e86a0eadd8aeb52b802335911ea8cfea1f07c9e
= <=>
2026-07-11 18:05:50 -0500

    Se agrego true

--- src/main/java/pe/cibertec/entities/Usuario.java
+++ src/main/java/pe/cibertec/entities/Usuario.java
@@ -2,6 +2,7 @@ package pe.cibertec.entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.engine.internal.Nullability;
 
 @Data
 @Entity
@@ -10,7 +11,13 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = true)
     private String nombre;
+
+    @Column(nullable = true)
     private String correo;
+
+    @Column(nullable = true)
     private String clave;
 }
