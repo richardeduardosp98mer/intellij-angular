55367f0d53fa831c216e8cba935f183a106052e2
= <=>
2026-07-11 17:48:40 -0500

    Rollback de lo que hizo Max

---
+++ src/main/java/pe/cibertec/entities/Usuario.java
@@ -0,0 +1,16 @@
+package pe.cibertec.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String correo;
+    private String clave;
+}
